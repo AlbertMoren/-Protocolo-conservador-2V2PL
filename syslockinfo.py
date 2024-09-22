9bb3ef7a80cee79adbeb26218df87d3872c62406
AlbertMoren <albertmorenn@gmail.com>
2024-09-22 19:54:53 -0300

    create getlock

--- syslockinfo.py
+++ syslockinfo.py
@@ -8,11 +8,15 @@ class LockTable:
     def __init__(self):
         self.locks = []
 
-    
-    def add_lock(self, transaction_id,granulosidade, tupla_id, tipo_bloqueio, status):
-        lock_id = len(self.locks) + 1  
+    def getLock(self,lock_id):
+        for lock in self.locks:
+            if lock[0] == lock_id:
+                return lock
+        
 
-        lock_entry = [lock_id, transaction_id, granulosidade, tupla_id, tipo_bloqueio, status]
+    def add_lock(self, transaction_id,granulosidade, objeto_id, tipo_bloqueio, status):
+        lock_id = len(self.locks) + 1  
+        lock_entry = [lock_id, transaction_id, granulosidade, objeto_id, tipo_bloqueio, status]
         self.locks.append(lock_entry)
 
     def remove_lock(self, lock_id):
@@ -32,22 +36,24 @@ class LockTable:
         time.sleep(2)
         self.clear_screen()
 
-        header = f"{'LockID':<8} {'TransactionID':<14} {'Granulosidade':<14} {'Tupla ID':<10} {'Tipo Bloqueio':<15} {'Status':<12}"
+        header = f"{'LockID':<8} {'TransactionID':<14} {'Granulosidade':<14} {'objeto_id':<10} {'Tipo Bloqueio':<15} {'Status':<12}"
         print(header)
         print("-" * len(header))  
         
        
         for lock in self.locks:
-            lock_id, transaction_id, granulosidade, tupla_id, tipo_bloqueio, status = lock
-            print(f"{lock_id:<8} {transaction_id:<14} {granulosidade:<14} {tupla_id:<10} {tipo_bloqueio:<15} {status:<12}")
+            lock_id, transaction_id, granulosidade, objeto_id, tipo_bloqueio, status = lock
+            print(f"{lock_id:<8} {transaction_id:<14} {granulosidade:<14} {objeto_id:<10} {tipo_bloqueio:<15} {status:<12}")
 
 
 lock_table_instance = LockTable()
 
 
-lock_table_instance.add_lock('2',f'{utils.TABELA}', '2', f'{utils.LEITURA}', f'{utils.CONCEDIDO}')
+lock_table_instance.add_lock('2',f'{utils.TUPLA}', '2', f'{utils.LEITURA}', f'{utils.CONCEDIDO}')
 lock_table_instance.add_lock('3',f'{utils.TUPLA}', '3', f'{utils.ESCRITA}', f'{utils.AGUARDANDO}')
-lock_table_instance.add_lock('4',f'{utils.PAGINA}', '4', f'{utils.LEITURA}', f'{utils.CONCEDIDO}')
+lock_table_instance.add_lock('4',f'{utils.TUPLA}', '4', f'{utils.LEITURA}', f'{utils.CONCEDIDO}')
 
 
 lock_table_instance.show_locks()
+
+print(lock_table_instance.getLock(1))
